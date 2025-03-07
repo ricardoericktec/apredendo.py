49218b2a3902abf299c8811cffd9ec69617b2aa6
ricardoericktec <ricardoerickpereirateodosiotec@gmail.com>
2025-03-07 09:34:34 -0300

    nova aula adicionada

---
+++ testes curso em video/aula10.py
@@ -0,0 +1,18 @@
+nome = input("qual seu nome: ")
+if nome == "Gustavo":
+    print("Que nome lindo você tem!")
+else:
+    print(f"bom dia {nome}l!")
+
+
+# ponto2
+
+n1 = float(input("digite a primeira nota: "))
+n2 = float(input("digite a segunda nota: "))
+m = (n1 + n2) / 2
+print(f"sua media foi {m}")
+if m >= 6.0:
+    print("sua media foi boa!")
+else:
+    print("sua media foi ruim!")
+    
